b2b8c28fe398f11b83556194e43a1e02542d06cb
Alejandro Niño <alejandro.nino@peaku.io>
2024-01-15 14:28:38 -0400

    fixing problems

--- src/scraping/scraping.service.ts
+++ src/scraping/scraping.service.ts
@@ -229,7 +229,11 @@ export class ScrapingService {
 
     async scrapeGetManfred(): Promise<any> {
         const browser = await chromium.launch({
-            headless: true
+            headless: true,
+            args: [
+                '--no-sandbox',
+                `--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36`,
+            ],
         });
 
         const context = await browser.newContext();
