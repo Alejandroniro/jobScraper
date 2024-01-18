f44b9ebbbd44c7216a740ef3440389c57f354d2f
Alejandro Niño <alejandro.nino@peaku.io>
2024-01-18 10:13:45 -0400

    fixing cors problem

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const options = new DocumentBuilder()
     .setTitle('Tu API Title')
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
